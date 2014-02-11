fc93dce39b370e751f5eda961ebf73013c7dc890
Peter M. Elias <petermelias@gmail.com>
2014-02-11 13:24:16 -0600

    cleaned up setup.py and version bump

--- setup.py
+++ setup.py
@@ -3,31 +3,36 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='assetman',
-	version='0.0.2',
-	url='http://github.com/petermelias/assetman',
-	license='BSD',
-	author='Peter M. Elias',
-	author_email='petermelias@gmail.com',
-	description='Generic blob-data asset manager that provides an API, CLI interface, for managing S3 assets and the respective CloudFront delivery setup.',
-	long_description=__doc__,
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	platforms='any',
-	install_requires=[
-		'boto',
-	],
-	extras_require={
-		'test': ['nose', 'wand', 'coveralls']
-	},
-	classifiers=[
-		'Environment :: Web Environment',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: MIT License',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-		'Topic :: Software Development :: Libraries :: Python Modules'
-	],
+    name='assetman',
+    version='0.0.3',
+    url='http://github.com/petermelias/assetman',
+    license='BSD',
+    author='Peter M. Elias',
+    author_email='petermelias@gmail.com',
+    description='Generic blob-data asset manager that provides an '
+                'API, CLI interface, for managing S3 assets and '
+                'the respective CloudFront delivery setup.',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'boto',
+    ],
+    extras_require={
+        'test': [
+            'coveralls',
+            'nose',
+            'wand',
+        ]
+    },
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
 )
